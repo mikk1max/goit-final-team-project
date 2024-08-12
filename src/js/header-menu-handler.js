35772936f9eeb671c582da4f74d114480531842b
Maksym Shepeta <maksim.shepeta@gmail.com>
2024-08-12 21:49:41 +0200

    fixing bug in js

--- src/js/header-menu-handler.js
+++ src/js/header-menu-handler.js
@@ -2,7 +2,9 @@ const btnOpen = document.querySelector('.burger-btn');
 const btnClose = document.querySelector('.btn-close-header-menu');
 const menuSection = document.querySelector('.header-menu-section');
 
-const buttons = document.querySelectorAll('#about-link, #yachts-link, #reviews-link');
+const btnAboutMenu = document.querySelector('#about-link');
+const btnYachtsMenu = document.querySelector('#yachts-link');
+const btnReviewsMenu = document.querySelector('#reviews-link');
 
 btnOpen.addEventListener('click', () => {
     menuSection.classList.add('is-open');
@@ -13,8 +15,14 @@ btnClose.addEventListener('click', () => {
 });
 
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        menuSection.classList.remove('is-open');
-    });
-});
+btnAboutMenu.addEventListener('click', () => {
+    menuSection.classList.remove('is-open');
+})
+
+btnYachtsMenu.addEventListener('click', () => {
+    menuSection.classList.remove('is-open');
+})
+
+btnReviewsMenu.addEventListener('click', () => {
+    menuSection.classList.remove('is-open');
+})
