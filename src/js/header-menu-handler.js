dfd7f8a661d941d6752b5ed0a141a581244a779f
Maksym Shepeta <maksim.shepeta@gmail.com>
2024-08-12 21:37:33 +0200

    update js for nav-btns

--- src/js/header-menu-handler.js
+++ src/js/header-menu-handler.js
@@ -2,10 +2,19 @@ const btnOpen = document.querySelector('.burger-btn');
 const btnClose = document.querySelector('.btn-close-header-menu');
 const menuSection = document.querySelector('.header-menu-section');
 
+const buttons = document.querySelectorAll('#about-link, #yachts-link, #reviews-link');
+
 btnOpen.addEventListener('click', () => {
     menuSection.classList.add('is-open');
 });
 
 btnClose.addEventListener('click', () => {
     menuSection.classList.remove('is-open');
+});
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        menuSection.classList.remove('is-open');
+    });
 });
