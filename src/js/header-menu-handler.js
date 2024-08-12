b26ad395385ec862fd3d2bced7d23470579f205c
Maksym Shepeta <maksim.shepeta@gmail.com>
2024-08-12 21:54:44 +0200

    fixed js

--- src/js/header-menu-handler.js
+++ src/js/header-menu-handler.js
@@ -1,11 +1,14 @@
+// Variables for burger-menu
 const btnOpen = document.querySelector('.burger-btn');
 const btnClose = document.querySelector('.btn-close-header-menu');
 const menuSection = document.querySelector('.header-menu-section');
 
+// Variables for mobile nav-menu
 const btnAboutMenu = document.querySelector('#about-link');
 const btnYachtsMenu = document.querySelector('#yachts-link');
 const btnReviewsMenu = document.querySelector('#reviews-link');
 
+// burger-menu
 btnOpen.addEventListener('click', () => {
     menuSection.classList.add('is-open');
 });
@@ -14,7 +17,7 @@ btnClose.addEventListener('click', () => {
     menuSection.classList.remove('is-open');
 });
 
-
+// mobile nav-menu
 btnAboutMenu.addEventListener('click', () => {
     menuSection.classList.remove('is-open');
 })
